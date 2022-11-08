b77e683fa59bae924c61b994a6b6b8b86606e08e
charlessimonyi <cfanhacker@gmail.com>
2022-11-08 22:11:32 +0800

    remove eosapi

--- eosapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from .exceptions import EosApiException, NodeException, TransactionException
-from .transaction import Account, Authorization, Action, Transaction
-from .eosapi import EosApi
-from .__version__ import __version__
